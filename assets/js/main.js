a52e8b5ef257ddee7d58564762d6c7e8030d7395
Eric Morales <ericdaniel3103@gmail.com>
2021-05-29 19:44:09 -0500

    Probando smooth scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,24 @@ d.addEventListener('DOMContentLoaded', e => {
     scrollTopButton(".scroll__top");
     darkTheme('#theme-button', 'dark-theme', '.hero-light', '.button-light');
 })
+
+document.addEventListener('DOMContentLoaded', function() {
+    scrollNav();
+})
+
+function scrollNav() {
+    const scrollTop = document.querySelector('.scroll__top');
+
+    const enlaces = document.querySelectorAll('.nav__link');
+
+    enlaces.forEach(function(enlace) {
+        enlace.addEventListener('click', function(e) {
+            e.preventDefault();
+            const seccion = document.querySelector(e.target.attributes.href.value);
+
+            seccion.scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+}
